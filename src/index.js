db25a00d5d248cd999351fa9673edfeead22d92d
Tarabrin Evgeniy <mistersviter@gmail.com>
2021-03-10 21:50:55 +0400

    feat: add 3 funtions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length && array.length) {
+        let minimum = Infinity;
+        for (let i = 0; i < array.length; i++) {
+            minimum = (array[i] < minimum) ? array[i] : minimum;
+        }
+        return minimum;
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length && array.length) {
+        let maximum = -Infinity;
+        for (let i = 0; i < array.length; i++) {
+            maximum = (array[i] > maximum) ? array[i] : maximum;
+        }
+        return maximum;
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length && array.length) {
+        let average = 0;
+        let summary = array.reduce(function (sum, current) {
+            return sum + current;
+        }, 0);
+        average = summary / array.length;
+        return average;
+    } else {
+        return 0;
+    }
 }
